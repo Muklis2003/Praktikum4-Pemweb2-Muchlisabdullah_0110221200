354a03c40147712ff3424ad5dedb9487f96f2125
Muklis2003 <91894493+Muklis2003@users.noreply.github.com>
2022-03-21 22:30:56 +0700

    Delete data_persegi_panjang.php

--- data_persegi_panjang.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once ('class_persegi_panjang.php');
-
-    $persegipanjang = new persegipanjang(10,16);
-
-    echo '<br>Luas Persegi Panjang : '.$persegipanjang->getLuas();
-    echo '<br>Keliling Persegi Panjang : '.$persegipanjang->getKeliling();
-?>
